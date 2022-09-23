c573138709c461f89c36121b199e23b33e9fc215
Вероника Михнюк <veronika.mikhnuyk@gmail.com>
2022-09-23 16:07:19 +0300

    Added event handler for each car-button

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,3 +1,9 @@
 document.getElementById("main-action").onclick = function () {
     document.getElementById("cars").scrollIntoView({behavior: "smooth"});
 }
+var buttons = document.getElementsByClassName("car-button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons [i].onclick = function () {
+        document.getElementById("price").scrollIntoView({behavior: "smooth"});
+    }
+}
