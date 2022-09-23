3d86a2dd67b40643583cb9aa9f028df92103d331
Вероника Михнюк <veronika.mikhnuyk@gmail.com>
2022-09-23 20:15:46 +0300

    Added animation effects to cars and pictures

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -34,3 +34,14 @@ document.getElementById("price-action").onclick = function () {
         });
     }
 }
+document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector('.price-image');
+    document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.3) / 20) + 'px,' + ((event.clientY * 0.3) / 20) + 'px,0px)';
+    });
+
+    const elem = document.querySelector(".main");
+    document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' + (0.2 * window.pageYOffset) + 'px';
+    })
+});
